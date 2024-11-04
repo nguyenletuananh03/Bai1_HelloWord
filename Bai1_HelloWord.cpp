fb98e6371a6ea4ea7576f90f7836b0e4fc485e53
nguyenletuananh03 <phuc98181>
2024-11-04 11:26:03 +0700

    Add project files.

---
+++ Bai1_HelloWord.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+{
+	cout << "hello worlds!\n";
+	cout << "toi ten la" << "\tuyen le tuan anh" << endl;
+	cout << "toi dang hoc \t+" << endl;
+	return 0;
+}
